e31cc39ecc2041d09cc4b5b44cbdf54e9616cebc
Johnata Marcelo Viana e Viana <johnata.viana@ecustomize.local>
2025-04-02 17:39:18 -0300

    Implementacao do caso de teste para o formulario Enter Insurant Data

--- Tests/TricentisTests.cs
+++ Tests/TricentisTests.cs
@@ -1,14 +1,16 @@
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using TricentisTest.Steps;
 
-
-namespace TricentisTest.Steps
+namespace TricentisTest.Tests
 {
     [TestFixture]
     public class TricentisTests
     {
         private IWebDriver driver;
         private VehicleDataSteps vehicleDataSteps;
+        private InsurantDataSteps insurantDataSteps;
 
         [SetUp]
         public void Setup()
@@ -18,12 +20,14 @@ namespace TricentisTest.Steps
             driver.Navigate().GoToUrl("http://sampleapp.tricentis.com/101/app.php");
 
             vehicleDataSteps = new VehicleDataSteps(driver);
+            insurantDataSteps = new InsurantDataSteps(driver);
         }
 
         [Test]
-        public void Test_FillVehicleData()
+        public void Test_FillVehicleAndInsurantData()
         {
             vehicleDataSteps.FillVehicleData();
+            insurantDataSteps.FillInsurantData();
         }
 
         [TearDown]
